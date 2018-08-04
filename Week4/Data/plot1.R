722ef09ca323176840d08199ab4e7dc98626dd14
remembrance1 <javierngkh@gmail.com>
2018-08-04 12:37:35 +0800

    Tidying

--- Week4/Data/plot1.R
+++
@@ -1,5 +0,0 @@
-#make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008
-NEI$SCC <- as.factor(NEI$SCC)
-totals <- aggregate(Emissions ~ year,NEI, sum) #summing emissions by SCC and year
-
-barplot(totals$Emissions, ylab = "PM2.5 Emissions", xlab = "Year", names.arg = totals$year, main = "PM2.5 Emission Totals")
